f12bbbd52e48b6025aa9fa5c45c0408411a73493
Shubham Sharma <shubhamsharma10112003@gmail.com>
2025-03-24 02:44:32 +0530

    corrected the userSchema file

--- Server/schema/userSchema.js
+++ Server/schema/userSchema.js
@@ -1,22 +1,12 @@
 const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-    fullName: {
-        type: String,
-        required: true,
+const userSchema = new mongoose.Schema(
+    {
+      fullname: { type: String, required: true },
+      email: { type: String, required: true, unique: true }, // Email must be unique
+      password: { type: String, required: true },
+      username: { type: String, unique: true, sparse: true }, // Allow null or undefined values
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 6,
-    },
-}, { timestamps: true });
-
-const User = mongoose.model("User", userSchema);
-
-module.exports = User;
+    { timestamps: true }
+  );
+  
+  module.exports = mongoose.model("User", userSchema);
