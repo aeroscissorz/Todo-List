fde8e3a5c633f76e8971fa5c69eb85227dbf3f9d
Shubham Sharma <shubhamsharma10112003@gmail.com>
2025-03-24 23:54:59 +0530

    small typo fix

--- Server/server.js
+++ Server/server.js
@@ -27,7 +27,7 @@ app.use("/todos",todoRoutes);
 
 app.use(express.static(path.join(__dirname, "../Client/dist")));
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "Client/dist", "index.html"));
+	res.sendFile(path.join(__dirname, "../Client/dist", "index.html"));
 });
 
 connectDB().then(()=>{
